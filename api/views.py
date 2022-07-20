d33339f93e85c6be1b19fa18190e6b8091faf85e
jenish-codes <jenish.codes@gmail.com>
2022-07-20 16:52:40 +0530

    REST api created

--- api/views.py
+++ api/views.py
@@ -1,1 +1,58 @@
+import imp
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+
+from students.models import Student
+from api.serializer import StudentSerializer
+
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+
+
+
+
+
+
+
+
+# function based method 1
+@csrf_exempt
+def students_list(request):
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = StudentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def student(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = StudentSerializer(student)
+        return JsonResponse(serializer.data)
+    
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = StudentSerializer(student, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == "DELETE":
+        student.delete()
+        return HttpResponse(status=204)
